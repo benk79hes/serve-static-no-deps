300aac6eb554c9f4663739874f2ad723f9ae959e
Benjamin Keller <bkeller79@hotmail.com>
2023-05-31 09:50:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Serve static with no dependencies
+
+## Scope
+
+The scope of this package is to serve static files from a directory
+as efficiently as possible and shall include no package dependencies
+except nodejs core packages.
+
+It can be used as middleware from express or express-like applications
+(e.g. Polka).
+
---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const path = require('path')
+
+//
+const MIME_TYPES = {
+    default: "application/octet-stream",
+    html: "text/html; charset=UTF-8",
+    js: "application/javascript",
+    css: "text/css",
+    png: "image/png",
+    jpg: "image/jpg",
+    gif: "image/gif",
+    ico: "image/x-icon",
+    svg: "image/svg+xml",
+    webmanifest: "application/manifest+json",
+};
+
+
+/**
+ * 
+ * @param {*} root It is the root folder of the static files to serve 
+ * @param {*} urlFolder It is the path to use as url base
+ * @returns a middleware function to be used by express js or such
+ */
+module.exports = function(root, urlFolder="", defaultIndex="index.html") {
+    return async function(req, res, next) {
+        
+		let urlPath = urlFolder + '/';
+		
+        if (req.url.indexOf(urlPath) !== 0) {
+            return next();
+        }
+
+        const localUrl = req.url.substr(urlPath.length + 1);
+		console.log(localUrl);
+        
+        let filePath = null;
+        if (localUrl.endsWith("/")) {
+            filePath = path.join(root, localUrl, defaultIndex)
+        }
+        else {
+            filePath = path.join(root, localUrl)
+        }
+		
+		console.log(filePath);
+
+        // requested path is outside of root
+        if (!filePath.startsWith(root)) {
+            return next();
+        }
+
+        const exists = await fs.promises.access(filePath)
+            .then(() => true, () => false);
+
+        if (!exists) {
+            return next();
+        }
+
+        const ext = path.extname(filePath).substring(1).toLowerCase();
+        const stream = fs.createReadStream(filePath);
+
+
+        const mimeType = MIME_TYPES[ext] || MIME_TYPES.default;
+        res.writeHead(200, { "Content-Type": mimeType });
+        stream.pipe(res);
+    }
+}
